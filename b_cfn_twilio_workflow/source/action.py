9689238fef3601a8c49c09a70ec3693772a00dda
Robertas Stankevičius <robertas.stankevicius@biomapas.com>
2020-10-30 11:34:12 +0200

    Add support for more updateable parameters.

--- b_cfn_twilio_workflow/source/action.py
+++ b_cfn_twilio_workflow/source/action.py
@@ -3,6 +3,7 @@ import logging
 import os
 from typing import Dict, Any, Optional, Tuple
 
+from twilio.base import values
 from twilio.base.exceptions import TwilioException
 from twilio.rest import Client
 
@@ -19,11 +20,17 @@ class Action:
             self.TWILIO_ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID']
             self.TWILIO_AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
             self.TWILIO_WORKSPACE_SID = os.environ['TWILIO_WORKSPACE_SID']
-            self.TWILIO_TASK_QUEUE_SID = os.environ['TWILIO_TASK_QUEUE_SID']
+
             self.TWILIO_WORKFLOW_NAME = self.__parameters['TwilioWorkflowName']
         except KeyError as ex:
             logger.error(f'Missing environment: {repr(ex)}.')
             raise
+
+        # Optional parameters
+        self.WORKFLOW_CONFIGURATION = self.__parameters.get('WorkflowConfiguration') or values.unset
+        self.ASSIGNMENT_CALLBACK_URL = self.__parameters.get('AssignmentCallbackUrl') or values.unset
+        self.FALLBACK_ASSIGNMENT_CALLBACK_URL = self.__parameters.get('FallbackAssignmentCallbackUrl') or values.unset
+        self.TASK_RESERVATION_TIMEOUT = self.__parameters.get('TaskReservationTimeout') or values.unset
 
         self.client = self.__get_twilio_client(self.TWILIO_ACCOUNT_SID, self.TWILIO_AUTH_TOKEN)
 
@@ -38,15 +45,11 @@ class Action:
         logger.info(f'Initiating resource creation with these parameters: {json.dumps(self.__parameters)}.')
 
         workflow = self.client.taskrouter.workspaces.get(sid=self.TWILIO_WORKSPACE_SID).workflows.create(
-            self.TWILIO_WORKFLOW_NAME,
-            json.dumps({
-                'task_routing': {
-                    'filters': [],
-                    'default_filter': {
-                        'queue': self.TWILIO_TASK_QUEUE_SID
-                    }
-                }
-            })
+            friendly_name=self.TWILIO_WORKFLOW_NAME,
+            configuration=self.WORKFLOW_CONFIGURATION,
+            assignment_callback_url=self.ASSIGNMENT_CALLBACK_URL,
+            fallback_assignment_callback_url=self.FALLBACK_ASSIGNMENT_CALLBACK_URL,
+            task_reservation_timeout=self.TASK_RESERVATION_TIMEOUT
         )
 
         workflow_sid = workflow.sid
@@ -64,10 +67,15 @@ class Action:
         logger.info(f'Initiating resource update with these parameters: {json.dumps(self.__parameters)}.')
 
         workflow_sid = self.__resource_id
-        workflow = self.client.taskrouter.workspaces.get(self.TWILIO_WORKSPACE_SID).workflows.get(workflow_sid).fetch()
+        workflow = self.client.taskrouter.workspaces.get(self.TWILIO_WORKSPACE_SID).workflows.get(workflow_sid)
 
-        if workflow.friendly_name != self.TWILIO_WORKFLOW_NAME:
-            workflow.update(friendly_name=self.TWILIO_WORKFLOW_NAME)
+        workflow.update(
+            friendly_name=self.TWILIO_WORKFLOW_NAME,
+            configuration=self.WORKFLOW_CONFIGURATION,
+            assignment_callback_url=self.ASSIGNMENT_CALLBACK_URL,
+            fallback_assignment_callback_url=self.FALLBACK_ASSIGNMENT_CALLBACK_URL,
+            task_reservation_timeout=self.TASK_RESERVATION_TIMEOUT
+        )
 
         return {'WorkflowSid': workflow_sid}, workflow_sid
 
